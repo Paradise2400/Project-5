92468d049baac9cc6614fee99b6dd114a4df1c23
Kevin Hoang <hevinkoang@gmail.com>
2021-11-23 16:32:39 -0800

    Partial DFT

--- src/main/ListGraph/Traversal.java
+++ src/main/ListGraph/Traversal.java
@@ -1,5 +1,14 @@
 package main.ListGraph;
-
 public class Traversal {
-            
+    public QueueInterface<Character> depthFirst(ListGraph<Character> graph){
+    QueueInterface<Character> traversalOrder = new LinkedQueue<Character>();
+    StackInterface<Character> vertexStack = new ResizableArrayStack<>(300);
+    String visited[];
+    char originVertex = graph.getLabel(0);
+    vertexStack.push(originVertex);
+    traversalOrder.enqueue(originVertex);
+    while(!vertexStack.isEmpty()){
+
+    }
+    }
 }
