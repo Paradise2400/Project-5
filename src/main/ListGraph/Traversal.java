62c3a2ebcb36d36cab3d4af55023e9e53806563f
Kevin Hoang <hevinkoang@gmail.com>
2021-11-23 19:45:15 -0800

    DFT implemented

--- src/main/ListGraph/Traversal.java
+++ src/main/ListGraph/Traversal.java
@@ -1,14 +1,31 @@
 package main.ListGraph;
+
+import main.List.List;
+
 public class Traversal {
     public QueueInterface<Character> depthFirst(ListGraph<Character> graph){
-    QueueInterface<Character> traversalOrder = new LinkedQueue<Character>();
-    StackInterface<Character> vertexStack = new ResizableArrayStack<>(300);
-    String visited[];
-    char originVertex = graph.getLabel(0);
-    vertexStack.push(originVertex);
-    traversalOrder.enqueue(originVertex);
-    while(!vertexStack.isEmpty()){
-
-    }
-    }
+        QueueInterface<Character> traversalOrder = new LinkedQueue<Character>();
+        StackInterface<Character> vertexStack = new ResizableArrayStack<>(300);
+        List<Character> visited = new List<>();
+        char originVertex = graph.getLabel(0);
+        vertexStack.push(originVertex);
+        traversalOrder.enqueue(originVertex);
+        int[] neighbors = graph.neighbors(0);
+        while(!vertexStack.isEmpty()){
+            char topVertex = vertexStack.peek();
+            visited.add(topVertex);
+            int min = neighbors[0];
+            for(int i = 1; i < neighbors.length; i++){
+                if(neighbors[i] < min)
+                    if(!visited.contains(graph.getLabel(neighbors[i])))
+                        min = neighbors[i];
+            }
+            neighbors = graph.neighbors(min);
+            vertexStack.push(graph.getLabel(min));
+            traversalOrder.enqueue(graph.getLabel(min));
+            if(neighbors.length == 0)
+                vertexStack.pop();
+        }
+        return traversalOrder;
+        }
 }
