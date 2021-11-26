1abd22ca3d4759eab9970dc35cb1962d1679e62f
Kevin Hoang <hevinkoang@gmail.com>
2021-11-26 12:19:07 -0800

    Updated queueinterface

--- src/main/ListGraph/QueueInterface.java
+++ src/main/ListGraph/QueueInterface.java
@@ -14,4 +14,8 @@ public interface QueueInterface<T>
   /** Detects whether this queue is empty.
    @return  True if the queue is empty, or false otherwise. */
   public boolean isEmpty();
+
+  /** Removes and returns the entry at the front of this queue.
+   @return  The object at the front of the queue.*/
+  public T dequeue() throws EmptyQueueException;
 } // end QueueInterface
