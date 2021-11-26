f2fe9935dbf8346cc9f88477754579eda920e8ca
Kevin Hoang <hevinkoang@gmail.com>
2021-11-26 12:32:00 -0800

    Unit test for listGraphTraversal

--- src/test/ListGraphTraversalTest.java
+++ src/test/ListGraphTraversalTest.java
@@ -1,8 +1,48 @@
 package test;
 
+import main.ListGraph.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import main.GraphCreation;
+
+
 public class ListGraphTraversalTest {
-    
+    @Test
+    void LTravesalTest() throws EmptyQueueException {
+        ListGraph<Character> graph = GraphCreation.createGivenListGraph();
+        QueueInterface<Character> result = LTraversal.depthFirst(graph);
+        QueueInterface<Character> test = new ArrayQueue<>();
+        test.enqueue('A');
+        test.enqueue('B');
+        test.enqueue('E');
+        test.enqueue('F');
+        test.enqueue('C');
+        test.enqueue('H');
+        test.enqueue('I');
+        test.enqueue('D');
+        test.enqueue('G');
+        //Order for both should be ABEFCHIDG
+        //Dequeue each to see if they match values
+        //Test for A
+        assertEquals(test.dequeue(), result.dequeue());
+        //Test for B
+        assertEquals(test.dequeue(), result.dequeue());
+        //Test for E
+        assertEquals(test.dequeue(), result.dequeue());
+        //Test for F
+        assertEquals(test.dequeue(), result.dequeue());
+        //Test for C
+        assertEquals(test.dequeue(), result.dequeue());
+        //Test for H
+        assertEquals(test.dequeue(), result.dequeue());
+        //Test for I
+        assertEquals(test.dequeue(), result.dequeue());
+        //Test for D
+        assertEquals(test.dequeue(), result.dequeue());
+        //Test for F
+        assertEquals(test.dequeue(), result.dequeue());
+    }
+
 }
+
